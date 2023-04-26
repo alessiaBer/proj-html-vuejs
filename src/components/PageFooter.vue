0840523a246baf5e0152cbd288531e37a652f060
alessiaBer <alexdeyn@gmail.com>
2023-04-26 17:31:30 +0200

    [add] dynamically footer menu

--- src/components/PageFooter.vue
+++ src/components/PageFooter.vue
@@ -1,16 +1,46 @@
 <script>
-import footer from '../data/footer'
+import { menu, footerNav, socials } from "../data/footer";
 export default {
-    name: 'PageFooter',
-    data() {
-        return {
-            footer
-        }
-    }
+  name: "PageFooter",
+  data() {
+    return {
+      menu,
+      footerNav,
+      socials,
+    };
+  },
 };
 </script>
 <template>
-  <footer></footer>
+  <footer>
+    <div class="footer-top">
+      <div class="my-container">
+        <div class="row row-cols-4">
+          <div class="col"></div>
+          <div class="col" v-for="list in menu">
+            <h5>{{ list.name }}</h5>
+            <ul>
+              <li v-for="item in list.items">{{ item }}</li>
+            </ul>
+          </div>
+          <div class="col"></div>
+        </div>
+        <div class="footer-nav">
+            <ul>
+                <!-- TODO fare una computed properties-->
+                <li v-for="item in footerNav" :class="item.active === true ? 'active' : ''">{{ item.name }}</li>
+            </ul>
+        </div>
+        <div class="socials">
+            <ul>
+                <li v-for="social in socials">
+                    <font-awesome-icon :icon="'fa-brands ' + social" />
+                </li>
+            </ul>
+        </div>
+      </div>
+    </div>
+  </footer>
 </template>
 
 <style lang="scss" scoped></style>
--- src/data/footer.js
+++ src/data/footer.js
@@ -1,52 +1,39 @@
-export default [
-    {
-        name: 'Shop by brand',
-        items: [
-            'Bed',
-            'Food',
-            'Toys',
-            'Transport'
-        ]
-    },
-    {
-        name: 'Useful links',
-        items: [
-            'My account',
-            'Orders',
-            'Checkout',
-            'Cart'
-        ]
-    },
-    {
-        items: [
-            {
-                name: 'Home',
-                active: true
-            },
-            {
-                name: 'About',
-                active: false
-            },
-            {
-                name: 'Blog',
-                active: false
-            },
-            {
-                name: 'Contact',
-                active: false
-            },
-            {
-                name: 'Shop',
-                active: false
-            }
-        ]
-    },
-    {
-        socials: [
-            'fa-twitter',
-            'fa-facebook-f',
-            'fa-instagram',
-            'fa-youtube'
-        ]
-    }
-]
+const menu = [
+  {
+    name: "Shop by brand",
+    items: ["Bed", "Food", "Toys", "Transport"],
+  },
+  {
+    name: "Useful links",
+    items: ["My account", "Orders", "Checkout", "Cart"],
+  },
+];
+
+const footerNav = [
+      {
+        name: "Home",
+        active: true,
+      },
+      {
+        name: "About",
+        active: false,
+      },
+      {
+        name: "Blog",
+        active: false,
+      },
+      {
+        name: "Contact",
+        active: false,
+      },
+      {
+        name: "Shop",
+        active: false,
+      },
+    ]
+
+const socials = ["fa-twitter", "fa-facebook-f", "fa-instagram", "fa-youtube"];
+
+export {menu}
+export {footerNav}
+export {socials}
