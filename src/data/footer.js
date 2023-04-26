879443c4b64c3f663353d585f4da6ada5b2ad8bf
alessiaBer <alexdeyn@gmail.com>
2023-04-26 17:06:14 +0200

    [create] footer data and assets

---
+++ src/data/footer.js
@@ -0,0 +1,52 @@
+export default [
+    {
+        name: 'Shop by brand',
+        items: [
+            'Bed',
+            'Food',
+            'Toys',
+            'Transport'
+        ]
+    },
+    {
+        name: 'Useful links',
+        items: [
+            'My account',
+            'Orders',
+            'Checkout',
+            'Cart'
+        ]
+    },
+    {
+        items: [
+            {
+                name: 'Home',
+                active: true
+            },
+            {
+                name: 'About',
+                active: false
+            },
+            {
+                name: 'Blog',
+                active: false
+            },
+            {
+                name: 'Contact',
+                active: false
+            },
+            {
+                name: 'Shop',
+                active: false
+            }
+        ]
+    },
+    {
+        socials: [
+            'fa-twitter',
+            'fa-facebook-f',
+            'fa-instagram',
+            'fa-youtube'
+        ]
+    }
+]
