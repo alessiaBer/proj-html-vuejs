4efc2aad7df3dd347c47988b6caa753c35bfd9e4
alessiaBer <alexdeyn@gmail.com>
2023-04-26 13:38:27 +0200

    [install] fontawesome

--- src/main.js
+++ src/main.js
@@ -1,5 +1,11 @@
 import { createApp } from 'vue'
 import './assets/scss/app.scss'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons'
+import { faUser } from '@fortawesome/free-regular-svg-icons'
+import { faBox } from '@fortawesome/free-solid-svg-icons'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+library.add(faMagnifyingGlass, faUser, faBox)
+createApp(App).component('font-awesome-icon', FontAwesomeIcon).mount('#app')
