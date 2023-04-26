9fd977cba5dcf42892ba81d4ab17141dad506067
alessiaBer <alexdeyn@gmail.com>
2023-04-26 16:17:04 +0200

    [install & import] fontsource

--- src/main.js
+++ src/main.js
@@ -5,7 +5,10 @@ import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons'
 import { faUser } from '@fortawesome/free-regular-svg-icons'
 import { faBox } from '@fortawesome/free-solid-svg-icons'
+import { faChevronDown } from '@fortawesome/free-solid-svg-icons'
+import '@fontsource/work-sans/400.css'
+import '@fontsource/work-sans/500.css'
 import App from './App.vue'
 
-library.add(faMagnifyingGlass, faUser, faBox)
+library.add(faMagnifyingGlass, faUser, faBox, faChevronDown)
 createApp(App).component('font-awesome-icon', FontAwesomeIcon).mount('#app')
