b2cef6726af9b7c68ae1511d5ab9ef71159d118b
alessiaBer <alexdeyn@gmail.com>
2023-04-27 16:00:29 +0200

    [add] jumbotron style and content

---
+++ src/components/JumboTron.vue
@@ -0,0 +1,55 @@
+<script>
+export default {
+  name: "JumboTron",
+};
+</script>
+<template>
+  <div class="jumbotron">
+    <div class="banner-top">
+      <div class="my_container">
+        <ul>
+          <li>
+            <font-awesome-icon icon="fa-regular fa-map" class="icon" />
+            <span><strong>International</strong> Shipping Services</span>
+          </li>
+          <li>
+            <font-awesome-icon icon="fa-regular fa-clock" class="icon" />
+            <span>See our <strong>store hours</strong> and services</span>
+          </li>
+          <li>
+            <font-awesome-icon icon="fa-solid fa-check" class="icon" />
+            <span
+              >We price match to give you the <strong>best deals</strong></span
+            >
+          </li>
+        </ul>
+      </div>
+    </div>
+    <div class="jumbo-main">
+        <div class="my_container">
+            <div class="text">
+               <span class="text-uppercase">Find the best animal supplies</span>
+            <h1>We know animals are a part of your family, let us help take care of them.</h1>
+            <button class="btn btn_light">
+                <a href="#" class="link_dark">Learn more about us</a>
+            </button> 
+            </div>
+            
+        </div>
+    </div>
+    <div class="banner-bottom">
+      <div class="my_container">
+        <img
+          src="../assets/img/images/food-transparent-5-200x112.png"
+          alt="Food"
+        />
+        <div class="text">
+          <span class="d-inline-block">Get $25 OFF your first purchase of our homemade pet food!</span>
+          <a href="#" class="link_light">Visit the shop</a>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped></style>
---
+++ src/components/PageMain.vue
@@ -0,0 +1,16 @@
+<script>
+import JumboTron from "./JumboTron.vue";
+export default {
+  name: "PageMain",
+  components: {
+    JumboTron,
+  },
+};
+</script>
+<template>
+  <main>
+    <JumboTron />
+  </main>
+</template>
+
+<style lang="scss" scoped></style>
