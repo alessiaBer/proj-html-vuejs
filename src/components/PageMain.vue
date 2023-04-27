14ade4639d5ed3d44f540b18541c11e2c3842a1e
alessiaBer <alexdeyn@gmail.com>
2023-04-27 17:10:37 +0200

    try to add img path

--- src/components/PageMain.vue
+++ src/components/PageMain.vue
@@ -1,15 +1,42 @@
 <script>
+import { categories } from "../data/main";
 import JumboTron from "./JumboTron.vue";
 export default {
   name: "PageMain",
   components: {
     JumboTron,
   },
+  data() {
+    return {
+      categories,
+    };
+  },
 };
 </script>
 <template>
   <main>
     <JumboTron />
+    <section>
+      <div class="my_container">
+        <h3>Browse by category</h3>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati cum
+          iusto adipisci eaque consectetur molestias voluptatibus illum
+          similique? Nostrum, et.
+        </p>
+        <div class="row row-cols-4">
+          <div class="col" v-for="category in categories">
+            <div class="my_card">
+              <img
+                :src="'../assets/img/images/' + category.img"
+                :alt="category.name"
+              />
+              <span>{{ category.name }} ({{ category.amount }})</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
   </main>
 </template>
 
---
+++ src/data/main.js
@@ -0,0 +1,24 @@
+const categories = [
+    {
+        name: 'Bed',
+        img: 'product-9-200x200.jpg',
+        amount: 1
+    },
+    {
+        name: 'Food',
+        img: 'product-3-150x150.jpg',
+        amount: 6
+    },
+    {
+        name: 'Toys',
+        img: 'product-2-150x150.jpg',
+        amount: 6
+    },
+    {
+        name: 'Transport',
+        img: 'product-4-150x150.jpg',
+        amount: 6
+    }
+]
+
+export {categories}
